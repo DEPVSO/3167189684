f65e8cab8716ff409b1eed8c836401dbcda3fa45
DEPVSO <129581340+DEPVSO@users.noreply.github.com>
2023-04-15 14:02:39 +0800

    Add files via upload

--- 4.5.c
+++ 4.5.c
@@ -41,10 +41,98 @@
 //	}
 //	return 0;
 //}
+//void switch_ (int* arr, int sz)
+//{
+//	int *left = arr;
+//	int* right = arr + sz - 1;
+//	int tem = 0;
+//	while (left < right)
+//	{
+//		while ((left < right) && (*left) % 2 == 1)
+//		{
+//			left++;
+//		}
+//		while ((left < right) && (*right) %2 == 0)
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			tem = *left;
+//			*left = *right;
+//			*right = tem;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,3,2,4,6,7,5,9,4,3 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	switch_ (arr, sz);
+//	int j=0;
+//	for (j = 0; j < sz; j++)
+//	{
+//		printf("%d ", arr[j]);
+//	}
+//	return 0;
+//}
+#include<string.h>
+#include<assert.h>
+//int juiny(char a[], char b[])
+//{
+//	int len=strlen(a);
+//	assert(strlen(b) == len);
+//	strncpy(a, a, len);
+//	return strstr(a, b)!=NULL;
+//}
+//int main()
+//{
+//	char a[30] = { "abcdef" };
+//	char b[] = { "bceaaa" };
+//	int tem=juiny(a, b);
+//	if (tem == 1)
+//		printf("YES\n");
+//	else if(tem==0)
+//		printf("NO\n");
+//	return 0;
+//}
+int look_up(int arr[][3], int* x, int* y, int k)
+{
+	
+	int lines= 0;
+	int ranks = *y-1;
+	while (lines < x && ranks >= 0)
+	{
+		if (arr[lines][ranks] == k)
+		{
+			*x = lines;
+			*y = ranks;
+			return 1;
+		}
+		else if (k > arr[lines][ranks])
+			lines++;
+		else
+			ranks--;
+	}
+	return 0;
+}
 int main()
 {
-	char* p = "abcdef";
-	
-	printf("%s", p);
+	int arr[4][3] = { {1,3,5},{2,4,6},{7,9,11},{8,10,12} };
+	int k=0;
+	int tem = 0;
+	int line = 4;
+	int rank = 3;
+	printf("请输入要查找的数:");
+	scanf("%d",&k);
+	tem=look_up(arr,&line,&rank,k);
+	if (tem == 1)
+	{
+		printf("找到了\n");
+		printf("下标是%d %d", line, rank);
+		
+	}
+	else
+		printf("找不到\n");
 	return 0;
 }
